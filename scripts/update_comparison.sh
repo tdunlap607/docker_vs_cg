ffb16af38292ed7ac1c793b9b5a518b792fe9dfa
tdunlap607 <tdunlap607@gmail.com>
2023-07-17 11:07:17 -0400

    Updates

--- scripts/update_comparison.sh
+++ scripts/update_comparison.sh
@@ -11,7 +11,7 @@ do
     # Docker prune
     docker image prune -f
 
-    # Pull the latest image
+
     docker pull $IMAGE:latest
 
     # Run grype on the freshly pulled docker image
@@ -42,6 +42,42 @@ do
     # generate SBOMs for each using syft
     syft cgr.dev/chainguard/$IMAGE:latest -o json > ./syft-output/$IMAGE-cg-sbom.json
     syft $IMAGE:latest -o json > ./syft-output/$IMAGE-sbom.json
+    syft $IMAGE-updated -o json > ./syft-output/$IMAGE-updated-sbom.json
+done
+
+for IMAGE in nginx:alpine-slim
+do
+    # Stop all containers
+    docker stop $(docker ps -a -q)
+
+    # Remove all containers
+    docker rm $(docker ps -a -q)
+
+    # Docker prune
+    docker image prune -f
+
+
+    docker pull $IMAGE
+
+    # Run grype on the freshly pulled docker image
+    grype $IMAGE -o json > ./grype-output/$IMAGE-grype.json
+
+    # Update packages within the docker image
+    # Try either apt or apk for distro type
+    docker run -u 0 -it $IMAGE /bin/sh -c "apt update && apt upgrade -y;apk update && apk upgrade --available;exit"
+
+    # Commit changes to a new updated docker image
+    docker commit $(docker ps -l --quiet) $IMAGE-updated
+
+    # Run grype on the new updated docker image
+    grype $IMAGE-updated -o json > ./grype-output/$IMAGE-updated-grype.json
+
+
+    ##############################################################################
+    ##############################################################################
+
+    # generate SBOMs for each using syft
+    syft $IMAGE -o json > ./syft-output/$IMAGE-sbom.json
     syft $IMAGE-updated -o json > ./syft-output/$IMAGE-updated-sbom.json
 done
 
